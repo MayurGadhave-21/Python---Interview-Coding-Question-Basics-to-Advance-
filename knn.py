817f4331ffa69e63753c25b618e4fb38a324e2a6
Mayur Gadhave <mbgadhave@mitaoe.ac.in>
2023-08-26 20:19:59 +0530

    Food App

--- knn.py
+++ knn.py
@@ -1,4 +1,48 @@
-x=10
-y=8
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
-assert x>y, 'X too small'
+# Load and preprocess the dataset
+data = pd.read_csv("Market_dataset.csv")
+X = data.drop("target_column", axis=1)
+y = data["target_column"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Artificial Neural Network (ANN) model
+ann_model = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=500)
+ann_model.fit(X_train_scaled, y_train)
+ann_predictions = ann_model.predict(X_test_scaled)
+ann_accuracy = accuracy_score(y_test, ann_predictions)
+ann_report = classification_report(y_test, ann_predictions)
+
+# K-Nearest Neighbors (KNN) model
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train_scaled, y_train)
+knn_predictions = knn_model.predict(X_test_scaled)
+knn_accuracy = accuracy_score(y_test, knn_predictions)
+knn_report = classification_report(y_test, knn_predictions)
+
+# Random Forest model
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train_scaled, y_train)
+rf_predictions = rf_model.predict(X_test_scaled)
+rf_accuracy = accuracy_score(y_test, rf_predictions)
+rf_report = classification_report(y_test, rf_predictions)
+
+# Print results
+print("ANN Accuracy:", ann_accuracy)
+print("KNN Accuracy:", knn_accuracy)
+print("Random Forest Accuracy:", rf_accuracy)
+
+print("ANN Classification Report:\n", ann_report)
+print("KNN Classification Report:\n", knn_report)
+print("Random Forest Classification Report:\n", rf_report)
